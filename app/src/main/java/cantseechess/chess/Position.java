6fb9a196883c449c991624e6717a77334f55d387
intrnlerr <nulltestb1@gmail.com>
2021-06-28 02:07:12 +0000

    implement Position deserialization with tests

--- app/src/main/java/cantseechess/chess/Position.java
+++ app/src/main/java/cantseechess/chess/Position.java
@@ -1,11 +1,36 @@
 package cantseechess.chess;
 
 public class Position {
-    Position(String position) {
+    private int rank;
+    private int file;
 
+    public Position(String position) {
+        if (position.length() < 2) {
+            throw new IllegalArgumentException();
+        }
+        // check if the file is valid
+        if (position.charAt(0) > 'h' || position.charAt(0) < 'a') {
+            throw new IllegalArgumentException();
+        }
+        // check if the rank is a valid number
+        if (position.charAt(1) > '8' || position.charAt(1) < '1') {
+            throw new IllegalArgumentException();
+        }
+        // there might be a better way to do this
+        file = position.charAt(0) - 'a';
+        rank = position.charAt(1) - '1';
     }
-    Position(char rank, int file) {
 
+    public Position(int rank, int file) {
+        this.rank = rank;
+        this.file = file;
     }
 
+    public int getFile() {
+        return file;
+    }
+
+    public int getRank() {
+        return rank;
+    }
 }
---
+++ app/src/test/java/cantseechess/PositionTest.java
@@ -0,0 +1,26 @@
+package cantseechess;
+
+import cantseechess.chess.Position;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThrows;
+
+public class PositionTest {
+    @Test
+    public void testConstructor() {
+        assertThrows(IllegalArgumentException.class, () -> new Position("a0"));
+        assertThrows(IllegalArgumentException.class, () -> new Position("q1"));
+        assertThrows(IllegalArgumentException.class, () -> new Position("a9"));
+        assertThrows(IllegalArgumentException.class, () -> new Position("hi"));
+        assertThrows(IllegalArgumentException.class, () -> new Position(""));
+
+        var p = new Position("a1");
+        assertEquals(0, p.getFile());
+        assertEquals(0, p.getRank());
+
+        p = new Position("b7");
+        assertEquals(1, p.getFile());
+        assertEquals(6, p.getRank());
+    }
+}
