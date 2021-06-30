cfa3e1551ad0a17ecdb2bb4770739239732a78fb
angryfirewizard <christianmbookout@gmail.com>
2021-06-30 15:12:32 -0700

    Initial

---
+++ app/src/main/java/cantseechess/chess/Untested.java
@@ -0,0 +1,11 @@
+package cantseechess.chess;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.SOURCE)
+@Target(ElementType.METHOD)
+public @interface Untested {
+}
