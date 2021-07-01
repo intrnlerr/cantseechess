f2cf0f7ae0cc5f9d9d4b4de646b737e4e48306e8
intrnlerr <nulltestb1@gmail.com>
2021-07-01 07:43:32 +0000

    more ChessGame tests

--- app/src/test/java/cantseechess/chess/ChessGameTest.java
+++ app/src/test/java/cantseechess/chess/ChessGameTest.java
@@ -46,6 +46,68 @@ public class ChessGameTest {
     }
 
     @Test
+    public void capture() {
+        var g = new ChessGame();
+        assertFalse(g.tryMove("exf3", Color.white));
+        assertFalse(g.tryMove("exf7", Color.white));
+        assertFalse(g.tryMove("exf7", Color.black));
+        assertFalse(g.tryMove("exf3", Color.black));
+    }
+
+    @Test
+    public void castling() {
+        var g = new ChessGame();
+        assertFalse(g.tryMove("O-O", Color.white));
+        assertFalse(g.tryMove("O-O", Color.black));
+        assertFalse(g.tryMove("O-O-O", Color.white));
+        assertFalse(g.tryMove("O-O-O", Color.black));
+    }
+
+    @Test
+    public void promotion() throws IncorrectFENException {
+        var g = new ChessGame("8/PPPPP3/8/8/8/8/ppppp3/8 w - - 0 1");
+        assertTrue(g.tryMove("a8=Q", Color.white));
+        assertTrue(g.tryMove("b8=N", Color.white));
+        assertTrue(g.tryMove("c8=R", Color.white));
+        assertTrue(g.tryMove("d8=B", Color.white));
+        assertFalse(g.tryMove("e8=K", Color.white));
+
+        assertTrue(g.tryMove("a1=Q", Color.black));
+        assertTrue(g.tryMove("b1=N", Color.black));
+        assertTrue(g.tryMove("c1=R", Color.black));
+        assertTrue(g.tryMove("d1=B", Color.black));
+        assertFalse(g.tryMove("e1=K", Color.black));
+    }
+
+    @Test
+    public void specificNotation() throws IncorrectFENException {
+        var g = new ChessGame("r3r3/6Q1/8/R7/8/2Q3Q1/8/R7 w - - 0 1");
+        assertTrue(g.tryMove("Qc3e5", Color.white));
+        assertTrue(g.tryMove("R1a3", Color.white));
+        assertTrue(g.tryMove("Rec8", Color.black));
+    }
+
+    @Test
+    public void enPassant() throws IncorrectFENException {
+        var g = new ChessGame("k7/8/8/1pP5/8/8/8/K7 w - b6 0 2");
+        assertTrue(g.tryMove("cxb6", Color.white));
+        // TODO: add more en passant cases
+    }
+
+    @Test
+    public void inCheck() throws IncorrectFENException {
+        var g = new ChessGame("7b/8/k7/6B1/8/1q6/8/K7 w - - 0 1");
+        assertFalse(g.tryMove("Bh4", Color.white));
+        assertFalse(g.tryMove("Bh6", Color.white));
+        assertFalse(g.tryMove("Bc1", Color.white));
+        assertFalse(g.tryMove("Be7", Color.white));
+        assertFalse(g.tryMove("Kb1", Color.white));
+        assertFalse(g.tryMove("Ka2", Color.white));
+        assertFalse(g.tryMove("Bb2", Color.white));
+        assertTrue(g.tryMove("Bf6", Color.white));
+    }
+
+    @Test
     public void whiteFirstMoves() {
         var g = new ChessGame();
         assertTrue(g.tryMove("e4", Color.white));
@@ -63,26 +125,10 @@ public class ChessGameTest {
         assertFalse(g.tryMove("Bb2", Color.white));
         assertFalse(g.tryMove("Bc2", Color.white));
         assertFalse(g.tryMove("Bc1", Color.white));
-
-
-    }
-
-    @Test
-    public void capture() {
-        var g = new ChessGame();
-        assertFalse(g.tryMove("exf3", Color.white));
-        assertFalse(g.tryMove("exf7", Color.white));
-        assertFalse(g.tryMove("exf7", Color.black));
-        assertFalse(g.tryMove("exf3", Color.black));
-    }
-
-    @Test
-    public void castling() {
-        var g = new ChessGame();
-        assertFalse(g.tryMove("O-O", Color.white));
-        assertFalse(g.tryMove("O-O", Color.black));
-        assertFalse(g.tryMove("O-O-O", Color.white));
-        assertFalse(g.tryMove("O-O-O", Color.black));
+        assertFalse(g.tryMove("Qd1", Color.white));
+        assertFalse(g.tryMove("Qd2", Color.white));
+        assertFalse(g.tryMove("Qe1", Color.white));
+        assertFalse(g.tryMove("Qc1", Color.white));
     }
 
     @Test
