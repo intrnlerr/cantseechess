26a4ce2f3ef1a53ba3536448480bee27c85f9da9
intrnlerr <nulltestb1@gmail.com>
2021-07-01 03:48:01 +0000

    add more tests to ChessGameTest :)

--- app/src/test/java/cantseechess/chess/ChessGameTest.java
+++ app/src/test/java/cantseechess/chess/ChessGameTest.java
@@ -2,8 +2,7 @@ package cantseechess.chess;
 
 import org.junit.Test;
 
-import static junit.framework.TestCase.assertEquals;
-import static junit.framework.TestCase.assertTrue;
+import static junit.framework.TestCase.*;
 
 public class ChessGameTest {
     @Test
@@ -44,5 +43,64 @@ public class ChessGameTest {
             assertTrue(b.getPiece(new Position(6, file)) instanceof Pawn);
             assertEquals(Color.black, b.getPiece(new Position(6, file)).getColor());
         }
+    }
+
+    @Test
+    public void whiteFirstMoves() {
+        var g = new ChessGame();
+        assertTrue(g.tryMove("e4", Color.white));
+        assertFalse(g.tryMove("e2", Color.white));
+        assertFalse(g.tryMove("f2", Color.white));
+        assertFalse(g.tryMove("e5", Color.white));
+        assertFalse(g.tryMove("e6", Color.white));
+        assertFalse(g.tryMove("e1", Color.white));
+        assertTrue(g.tryMove("Nc3", Color.white));
+        assertFalse(g.tryMove("Na1", Color.white));
+        assertFalse(g.tryMove("Nb1", Color.white));
+        assertFalse(g.tryMove("Nc2", Color.white));
+        assertFalse(g.tryMove("Kd1", Color.white));
+        assertFalse(g.tryMove("Ba1", Color.white));
+        assertFalse(g.tryMove("Bb2", Color.white));
+        assertFalse(g.tryMove("Bc2", Color.white));
+        assertFalse(g.tryMove("Bc1", Color.white));
+
+
+    }
+
+    @Test
+    public void capture() {
+        var g = new ChessGame();
+        assertFalse(g.tryMove("exf3", Color.white));
+        assertFalse(g.tryMove("exf7", Color.white));
+        assertFalse(g.tryMove("exf7", Color.black));
+        assertFalse(g.tryMove("exf3", Color.black));
+    }
+
+    @Test
+    public void castling() {
+        var g = new ChessGame();
+        assertFalse(g.tryMove("O-O", Color.white));
+        assertFalse(g.tryMove("O-O", Color.black));
+        assertFalse(g.tryMove("O-O-O", Color.white));
+        assertFalse(g.tryMove("O-O-O", Color.black));
+    }
+
+    @Test
+    public void blackFirstMoves() {
+        var g = new ChessGame();
+        assertTrue(g.tryMove("e5", Color.black));
+        assertFalse(g.tryMove("e4", Color.black));
+        assertTrue(g.tryMove("e6", Color.black));
+        assertFalse(g.tryMove("e7", Color.black));
+        assertFalse(g.tryMove("e8", Color.black));
+        assertTrue(g.tryMove("Nc6", Color.black));
+        assertFalse(g.tryMove("Na8", Color.black));
+        assertFalse(g.tryMove("Nb8", Color.black));
+        assertFalse(g.tryMove("Nc7", Color.black));
+        assertFalse(g.tryMove("Kd8", Color.black));
+        assertFalse(g.tryMove("Ba8", Color.black));
+        assertFalse(g.tryMove("Bb7", Color.black));
+        assertFalse(g.tryMove("Bc7", Color.black));
+        assertFalse(g.tryMove("Bc8", Color.black));
     }
 }
