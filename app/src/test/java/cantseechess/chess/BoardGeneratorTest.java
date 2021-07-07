0d65a9895759f4094491d640787d5a2575c238eb
intrnlerr <nulltestb1@gmail.com>
2021-07-07 02:42:04 +0000

    not very useful test for BoardGenerator

---
+++ app/src/test/java/cantseechess/chess/BoardGeneratorTest.java
@@ -0,0 +1,12 @@
+package cantseechess.chess;
+
+import org.junit.Test;
+
+import java.io.IOException;
+
+public class BoardGeneratorTest {
+    @Test
+    public void makeBoard() throws IOException, IncorrectFENException {
+        BoardGenerator.getBoard("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1");
+    }
+}
