1ef8ee250a7555eb91aa0e743977c0fd758f2258
intrnlerr <nulltestb1@gmail.com>
2021-07-07 02:41:31 +0000

    make BoardGenerator use getResource and the new sprites

--- app/src/main/java/cantseechess/chess/BoardGenerator.java
+++ app/src/main/java/cantseechess/chess/BoardGenerator.java
@@ -3,18 +3,16 @@ package cantseechess.chess;
 import javax.imageio.ImageIO;
 import java.awt.*;
 import java.awt.image.BufferedImage;
-import java.io.File;
 import java.io.IOException;
-import java.net.URL;
 
 public class BoardGenerator {
     private static BufferedImage wPawn, wKnight, wRook, wBishop, wQueen, wKing, bPawn, bKnight, bRook, bBishop, bQueen, bKing;
     private static boolean piecesInit = false;
-    private static final String PIECE_ARRAY_URL = System.getProperty("user.dir") + "\\app\\src\\main\\java\\cantseechess\\resources\\ChessPiecesArray.png";
-    private static final String BOARD_URL = System.getProperty("user.dir") + "\\app\\src\\main\\java\\cantseechess\\resources\\Chessboard.png";
+    private static final String PIECE_ARRAY_URL = "/ChessPiecesArray.png";
+    private static final String BOARD_URL = "/Chessboard.png";
     private static BufferedImage BOARD_IMAGE;
-    private static final int WIDTH = 60;
-    private static final int HEIGHT = 60;
+    private static final int PIECE_WIDTH = 128;
+    private static final int PIECE_HEIGHT = 128;
 
     public static final BufferedImage getBoard(String FEN) throws IOException, IncorrectFENException {
         return getBoard(ChessGame.FENtoBoard(FEN));
@@ -29,65 +27,60 @@ public class BoardGenerator {
             for (int j = 0; j < 8; j++) {
                 Piece p = board[i][j];
                 if (p instanceof Blank) continue;
-                Image img = getImage(p).getScaledInstance(BOARD_IMAGE.getWidth()/8, BOARD_IMAGE.getHeight()/8, Image.SCALE_DEFAULT);
-
-                gr.drawImage(img, BOARD_IMAGE.getWidth()/8 * i, BOARD_IMAGE.getHeight()/8 * (7-j), null);
+                Image img = getImage(p);
+                gr.drawImage(img, BOARD_IMAGE.getWidth() / 8 * i, BOARD_IMAGE.getHeight() / 8 * (7 - j), null);
             }
         }
         gr.dispose();
         return toReturn;
     }
+
     private static BufferedImage getImage(Piece p) {
         BufferedImage toReturn = null;
         if (p instanceof Blank) return null;
 
         if (p instanceof Pawn) {
             toReturn = p.getColor() == Color.black ? bPawn : wPawn;
-        }
-        else if (p instanceof Knight) {
+        } else if (p instanceof Knight) {
             toReturn = p.getColor() == Color.black ? bKnight : wKnight;
-        }
-        else if (p instanceof Bishop) {
+        } else if (p instanceof Bishop) {
             toReturn = p.getColor() == Color.black ? bBishop : wBishop;
-        }
-        else if (p instanceof Rook) {
+        } else if (p instanceof Rook) {
             toReturn = p.getColor() == Color.black ? bRook : wRook;
-        }
-        else if (p instanceof King) {
+        } else if (p instanceof King) {
             toReturn = p.getColor() == Color.black ? bKing : wKing;
-        }
-        else if (p instanceof Queen) {
+        } else if (p instanceof Queen) {
             toReturn = p.getColor() == Color.black ? bQueen : wQueen;
         }
         return toReturn;
     }
 
     private static void initializePieces() throws IOException {
-        BufferedImage pieceArray = ImageIO.read(new File(PIECE_ARRAY_URL));
-        BOARD_IMAGE = ImageIO.read(new File(BOARD_URL));
+        BufferedImage pieceArray = ImageIO.read(BoardGenerator.class.getResourceAsStream(PIECE_ARRAY_URL));
+        BOARD_IMAGE = ImageIO.read(BoardGenerator.class.getResourceAsStream(BOARD_URL));
         BufferedImage[] pieces = new BufferedImage[12];
         for (int i = 0; i < pieces.length; i++) {
-
-            pieces[i] = new BufferedImage(WIDTH, HEIGHT, pieceArray.getType());
+            pieces[i] = new BufferedImage(PIECE_WIDTH, PIECE_HEIGHT, pieceArray.getType());
 
             Graphics2D gr = pieces[i].createGraphics();
-            int widthMultiplier = i%6;
+            int widthMultiplier = i % 6;
             int heightMultiplier = i / 6;
-            gr.drawImage(pieceArray, 0, 0, WIDTH, HEIGHT, WIDTH * widthMultiplier, HEIGHT * heightMultiplier, WIDTH *  (widthMultiplier + 1), HEIGHT * (heightMultiplier + 1), null);
+            gr.drawImage(pieceArray, 0, 0, PIECE_WIDTH, PIECE_HEIGHT, PIECE_WIDTH * widthMultiplier, PIECE_HEIGHT * heightMultiplier, PIECE_WIDTH * (widthMultiplier + 1), PIECE_HEIGHT * (heightMultiplier + 1), null);
             gr.dispose();
         }
-        bQueen = pieces[0];
-        bKing = pieces[1];
-        bRook = pieces[2];
-        bKnight = pieces[3];
-        bBishop = pieces[4];
-        bPawn = pieces[5];
-        wQueen = pieces[6];
-        wKing = pieces[7];
-        wRook = pieces[8];
-        wKnight = pieces[9];
-        wBishop = pieces[10];
-        wPawn = pieces[11];
+
+        wKing = pieces[0];
+        wQueen = pieces[1];
+        wBishop = pieces[2];
+        wKnight = pieces[3];
+        wRook = pieces[4];
+        wPawn = pieces[5];
+        bKing = pieces[6];
+        bQueen = pieces[7];
+        bBishop = pieces[8];
+        bKnight = pieces[9];
+        bRook = pieces[10];
+        bPawn = pieces[11];
         piecesInit = true;
     }
 }
