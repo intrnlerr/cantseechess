a837a4257e0449ef3c5617b0aa534305ce08df17
angryfirewizard <christianmbookout@gmail.com>
2021-07-09 14:01:31 -0700

    analysis now works with consumers to update the analysis shown in the message as stockfish is calculating it

--- app/src/main/java/cantseechess/stockfish/Analysis.java
+++ app/src/main/java/cantseechess/stockfish/Analysis.java
@@ -1,64 +1,85 @@
 package cantseechess.stockfish;
 
 import java.io.*;
-import java.util.Arrays;
+import java.util.Timer;
+import java.util.TimerTask;
+import java.util.function.Consumer;
 
 //http://wbec-ridderkerk.nl/html/UCIProtocol.html stockfish stuff
 public class Analysis {
     private final BufferedReader stockfishReader;
     private final OutputStreamWriter stockfishWriter;
-    //The amount of time in milliseconds stockfish should take to determine analysis
-    private final int time = 1000;
-    public Analysis() throws IOException {
+    //The max amount of time stockfish should calculate the score
+    private final int maxTime = 5000;
+
+    private Timer timer = new Timer();
+    private Consumer<String> received;
+    private String FEN;
+    private boolean stop = false;
+    //send out an analysis which gets processed by the bot and the bot edits the message containing the analysis.
+    //if the (next) or (previous) buttons are clicked, then the Analysis class restarts and looks at those states instead.
+
+    public Analysis(String FEN, Consumer<String> received) throws IOException {
         Process fish = Runtime.getRuntime().exec(this.getClass().getClassLoader().getResource("stockfish.exe").getPath());
         InputStreamReader fishReader = new InputStreamReader(fish.getInputStream());
         stockfishReader = new BufferedReader(fishReader);
         stockfishWriter = new OutputStreamWriter(fish.getOutputStream());
+
+        this.FEN = FEN;
+        this.received = received;
+        run();
     }
 
+
+    public void stop() throws IOException {
+        send("stop");
+        stop = true;
+    }
+
+    public void restart(String FEN) throws IOException {
+        stop();
+        stop = false;
+        this.FEN = FEN;
+        run();
+    }
+
+    //Send string str to stockfish
     private void send(String str) throws IOException{
         stockfishWriter.write(str + "\n");
         stockfishWriter.flush();
     }
 
-    private String receive() throws IOException {
-        StringBuffer read = new StringBuffer();
-        String line;
-        try {
-            Thread.sleep(time + 20);
-        } catch (Exception e) {
+    //Runs the timer event, collecting the most recent analysis from stockfish
+    private void run() throws IOException{
+        send("position fen " + FEN);
+        send("go movetime " + maxTime);
 
+        long startTime = System.currentTimeMillis();
+        try {
+            String line;
+            while (!stop && (line = stockfishReader.readLine()) != null && System.currentTimeMillis() - startTime < maxTime) {
+                if (line.contains("bestmove")) break;
+                if (!line.contains("cp")) continue;
+                String cp = getScore(line);
+                received.accept(cp);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
         }
-        send("isready");
-        while (!(line = stockfishReader.readLine()).equals("readyok")) {
-            read.append(line + "\n");
-        }
-        return read.toString();
     }
 
-    public String analyze(String FEN) throws IOException {
-
-        send("position fen " + FEN);
-        send("go movetime " + time);
-
-        String read = receive();
-
-        String[] toParse = read.split("\n");
-
+    private String getScore(String line) {
         Integer cp = null;
-        //find centipawn score in the last
+        String[] toParse = line.split(" ");
         for (int i = 0; i < toParse.length; i++) {
-            String[] s = toParse[i].split(" ");
-            for (int ii = 0; ii < s.length; ii++) {
-                if (s[ii].equals("cp")) {
-                    cp = Integer.parseInt(s[ii + 1]);
-                    break;
-                }
+            if (toParse[i].equals("cp")) {
+                cp = Integer.parseInt(toParse[i + 1]);
+                break;
             }
         }
-        if (cp == null) return null;
+        if (cp == null || cp == 0) return "0.0";
         int multiplier = FEN.split(" ")[1].equals("w") ? 1 : -1;
-        double score = Math.round(multiplier * cp/10.0)/10.0;
+        double score = Math.round(multiplier * cp / 10.0) / 10.0;
         String symbol = score > 0 ? "+" : "";
         return symbol + score;
     }
