6d61f37e61e3a86539f81e488a240489e22e61ac
angryfirewizard <christianmbookout@gmail.com>
2021-07-19 15:15:57 -0700

    add challenger color and time control

--- app/src/main/java/cantseechess/Challenge.java
+++ app/src/main/java/cantseechess/Challenge.java
@@ -1,14 +1,21 @@
 package cantseechess;
 
 import cantseechess.chess.ChessGame;
+import cantseechess.chess.Color;
 
 public class Challenge {
     public final String challenger;
     public final String challenged;
+    public final Color challengerColor;
+    public final int time;
+    public final int increment;
 
-    public Challenge(String challenger, String challenged) {
+    public Challenge(String challenger, String challenged, Color challengerColor, int time, int increment) {
         this.challenger = challenger;
         this.challenged = challenged;
+        this.challengerColor = challengerColor;
+        this.time = time;
+        this.increment = increment;
     }
 
     public ChessGame accept() {
