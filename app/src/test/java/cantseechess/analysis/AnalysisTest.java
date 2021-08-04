64ee2107808fc746def3c4c02462f7a6d82deb60
intrnlerr <nulltestb1@gmail.com>
2021-08-04 21:50:30 +0000

    low quality test for analysis

---
+++ app/src/test/java/cantseechess/analysis/AnalysisTest.java
@@ -0,0 +1,30 @@
+package cantseechess.analysis;
+
+import cantseechess.chess.ChessGame;
+import cantseechess.chess.Color;
+import cantseechess.chess.IllegalMoveException;
+import cantseechess.stockfish.Analysis;
+import org.junit.Test;
+
+import java.util.ArrayList;
+
+import static org.junit.Assert.assertEquals;
+
+public class AnalysisTest {
+
+    @Test
+    public void analysis() throws IllegalMoveException {
+        var game = new ChessGame();
+        var m1 = game.getMove("e4", Color.white);
+        game.makeMove(m1);
+        var m2 = game.getMove("e5", Color.black);
+        var moves = new ArrayList<ChessGame.Move>();
+        moves.add(m1);
+        moves.add(m2);
+        var str = new ArrayList<String>();
+        var a = new Analysis(str::add);
+        a.setMoves(moves);
+        a.run();
+        assertEquals(3, str.size());
+    }
+}
